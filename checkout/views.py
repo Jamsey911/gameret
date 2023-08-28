0439333e96fcabc9f802cf5efbdf990d5265f686
Jamsey911 <jamesalynch911@hotmail.com>
2023-08-28 02:16:15 +0000

    Add inital stripe payment details

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-    }
+        'stripe_public_key': 'pk_test_51NUofrKWxlYc6JNxhTN88pa18iZgjK7OOg06Ev3yCjiRjTeF6xnd2Wy9CkfE9zo4d4co6gI2NR6MOBgf0noOlJKl00oicn7DWR',
+        'client_secret': 'test client secret',
+        }
 
     return render(request, template, context)
