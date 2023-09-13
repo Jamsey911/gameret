64a31d98e6801dc79d245c7a49b66bb3435760f0
Jamsey911 <jamesalynch911@hotmail.com>
2023-09-13 20:36:23 +0000

    Add inital message from for about page

--- about/views.py
+++ about/views.py
@@ -1,9 +1,53 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
-# Create your views here.
+# Internal:
+from .models import Contact
+from .forms import ContactForm
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 def about(request):
-    """ A view to return the index page """
+    """
+    view rendering contact form and
+    send mail functionality
+    """
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data['name']
+            user_message = form.cleaned_data['message']
+            subject = form.cleaned_data['inquiry_purpose']
+            message = render_to_string(
+                'about/confirmation_email/email_confirmation.txt', {
+                    'name': name,
+                    'message': user_message
+                })
+            email_from = settings.DEFAULT_FROM_EMAIL
+            email_to = [form.cleaned_data['email']]
 
-    return render(request, 'about/about.html', {})
+            send_mail(
+                subject,
+                message,
+                email_from,
+                email_to
+            )
+            messages.success(request, f'Your message was sent successfuly')
+            return redirect(reverse('about:about'))
+
+        else:
+            messages.error(request, f'An error occured please check the form')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'about/about.html', context)
