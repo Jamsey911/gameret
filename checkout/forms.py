c850ab17faa91e552914b9a4d07fb00030285469
Jamsey911 <jamesalynch911@hotmail.com>
2023-09-28 19:42:16 +0000

    Resolve Accessibility bugs for wave validation

--- checkout/forms.py
+++ checkout/forms.py
@@ -40,5 +40,7 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['aria-label'] = placeholders[
+                    field]
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
