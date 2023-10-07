fd992af39dadf3284082c4b61fc2cd5683f44237
Jamsey911 <jamesalynch911@hotmail.com>
2023-10-07 19:26:28 +0000

    Correct label for textfield in blog forms.py for accessibity

--- blog/forms.py
+++ blog/forms.py
@@ -10,5 +10,23 @@ class CommentForm(forms.ModelForm):
     Comment form for users wishing to comment on a post
     """
     class Meta:
+        """
+        Comment form for users wishing to comment on a post
+        """
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'body': '',
+        }
+
+        for field in self.fields:
+            if field != 'country':
+                self.fields[field].widget.attrs['aria-label'] = placeholders[
+                    field]
